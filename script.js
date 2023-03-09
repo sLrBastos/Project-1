47611c633804a1210663e629e2acebffc3558f50
sLrBastos <cerejobastosa7x@gmail.com>
2023-03-09 15:52:52 +0000

    resolve conflict

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".burgerBtn")
-const navMenu = document.querySelector(".nav-menu")
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
